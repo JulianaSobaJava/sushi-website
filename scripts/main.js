e527bcc2be137efd56de58db09230fd866a0987e
JulianaSobaJava <julianasobajava23@gmail.com>
2023-04-26 15:17:35 +0100

    :zap: general updates

--- scripts/main.js
+++ scripts/main.js
@@ -19,4 +19,12 @@ const navMenu = document.getElementById("nav-menu"),
         navMenu.classList.remove("show-menu")
       }
       navLink.forEach(n => n.addEventListener("click", linkAction))
+
+      /*===================Change Header Background scroll =======*/
+      const scrollHeader = ()=>{
+        const header = document.getElementById('header')
+        this.scrollY  >= 50 ? header.classList.add('bg-header')
+                            :  header.classList.remove('bg-header')
+      }
+      window.addEventListener('scroll', scrollHeader)
      
