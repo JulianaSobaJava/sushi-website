c7b3b91667b4267968f62b1c05c2c4f2eb0b702a
JulianaSobaJava <julianasobajava23@gmail.com>
2023-04-28 10:53:37 +0100

    :fire: last scripts

--- scripts/main.js
+++ scripts/main.js
@@ -28,3 +28,43 @@ const navMenu = document.getElementById("nav-menu"),
       }
       window.addEventListener('scroll', scrollHeader)
      
+
+       /*===================Change Theme =======*/
+      const themeButton = document.getElementById("theme-button");
+      const darkTheme = "dark_theme";
+      const iconTheme = "ri-sun-line";
+
+      const selectedTheme = localStorage.getItem("selected-theme");
+      const selectedIcon = localStorage.getItem("selected-icon");
+
+       const getCurrentTheme = document.body.classList.contains(darkTheme)? "dark": "light";
+       const getCurrentIcon = themeButton.classList.contains(iconTheme)? "ri-moon-line": "ri-sun-line"
+
+       if(selectedTheme){
+        document.body.classList[selectedTheme === "dark" ? "add": "remove"](darkTheme)
+        themeButton.classList[selectedIcon === "ri-moon-line" ? "add": "remove"](darkIcon)
+       }
+
+       themeButton.addEventListener("click", ()=>{
+        document.body.classList.toggle(darkTheme)
+        themeButton.classList.toggle(iconTheme)
+
+        localStorage.setItem("seleted-theme", getCurrentTheme());
+        localStorage.setItem("seleted-icon", getCurrentIcon());
+       })
+
+       /*=======================ScrollRevealAnimation================================*/
+       const src = ScrollReveal({
+        origin: 'top',
+        distance: '60px',
+        duration: 2000,
+        delay: 300,
+        //reset:  true
+       }
+       )
+
+       src.reveal(`.home_img, .newletter_container, .footer_logo, .footer_description, .footer_content, .footer_info`)
+       src.reveal(`.home_data`,{origin: 'bottom'})
+       src.reveal(`.about_data, .recently_data`,{origin: 'left'})
+       src.reveal(`.about_img, .recently_image`,{origin: 'right'})
+       src.reveal(`.popular_card`,{interval: 100})
